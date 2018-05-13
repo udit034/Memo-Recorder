d4fc7a3c8f0cabe7131e615a1b225aac88a45d3c
udit034 <udit.mehata@gmail.com>
2018-05-12 18:26:30 -0700

    Configured application. Created notesMemoController

---
+++ Controller/notesMemoController.js
@@ -0,0 +1,15 @@
+module.exports = function( application ){
+
+application.get("/notesMemo", function( request, response ){
+
+});
+
+application.post("/notesMemo", function( request, response ){
+
+});
+
+application.delete("/notesMemo", function( request, response ){
+
+});
+
+};
