945fedd39626417199bda0ba6af95ca9732856c2
udit034 <udit.mehata@gmail.com>
2018-05-25 15:59:42 -0700

    Added functionality to add and delete notes without persistence

--- Controller/notesMemoController.js
+++ Controller/notesMemoController.js
@@ -1,15 +1,26 @@
+var bodyParser = require('body-parser');
+
+var currentNotes = [ { noteText : " Learn NodeJS" }, { noteText : " Learn ExpressJS" },
+             { noteText : " Learn MongoDB " } ]
+
+var urlEncodedParser = bodyParser.urlencoded( {extended:false} );
+
 module.exports = function( application ){
 
 application.get("/notesMemoView", function( request, response ){
-    response.render('VisibleMemos');
+    response.render('VisibleMemos', {currentNotes:currentNotes} );
 });
 
-application.post("/notesMemo", function( request, response ){
-
+application.post("/notesMemoView", urlEncodedParser, function( request, response ){
+      currentNotes.push( { noteText :request.body.item } );
+      response.json( currentNotes );
 });
 
-application.delete("/notesMemo", function( request, response ){
-
+application.delete("/notesMemoView/:item", function( request, response ){
+    currentNotes = currentNotes.filter( function( currentNote ){
+          return currentNote.noteText.replace(/ /g, "-") != request.params.item;
+    });
+    response.json( currentNotes );
 });
 
 };
--- assets/todo-list.js
+++ assets/todo-list.js
@@ -3,29 +3,24 @@ $(document).ready(function(){
   $('form').on('submit', function(){
 
       var item = $('form input');
-      var todo = {item: item.val()};
-
+      var currentNote = {item: item.val()};
       $.ajax({
         type: 'POST',
-        url: '/todo',
-        data: todo,
-        success: function(data){
-          //do something with the data via front-end framework
+        url: '/notesMemoView',
+        data: currentNote,
+        success: function( data ){
           location.reload();
         }
       });
-
       return false;
-
   });
 
   $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
+      var noteSelected = $(this).text().replace(/ /g, "-");
       $.ajax({
         type: 'DELETE',
-        url: '/todo/' + item,
+        url: '/notesMemoView/' + noteSelected,
         success: function(data){
-          //do something with the data via front-end framework
           location.reload();
         }
       });
